59952836520d936f3ad2314f63ca0680864cbdf7
shano6 <datoshanidze@gmail.com>
2023-06-16 11:35:26 +0400

    added homework 4,5

---
+++ Davaleba 4/Task 2/Program.cs
@@ -0,0 +1,47 @@
+﻿char[] array = GetUserInputArray();
+
+Console.Write("Enter a symbol to search in the array: ");
+char symbol = Convert.ToChar(Console.ReadLine()!);
+
+int count = CountOccurrences(array, symbol);
+
+PrintOccurrences(symbol, count);
+
+char[] GetUserInputArray()
+{
+    Console.Write("Enter the size of the array: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    char[] array = new char[size];
+
+    Console.WriteLine("Enter the elements of the array:");
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Element {0}: ", i + 1);
+        array[i] = Convert.ToChar(Console.ReadLine()!);
+    }
+
+    return array;
+}
+int CountOccurrences(char[] array, char symbol)
+{
+    int count = 0;
+
+    foreach (char element in array)
+    {
+        if (element == symbol)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+void PrintOccurrences(char symbol, int count)
+{
+    Console.WriteLine("Symbol '{0}' was found {1} time(s).", symbol, count);
+}
+
+
